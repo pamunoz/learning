43805c6a10f5071397e72d1f353935c073444e9b
Pablo Munoz <pfariasmunoz@gmail.com>
2016-11-13 17:00:49 -0300

    Add refactor lambda with parameter

--- learning_java/learning_java/src/lambdaexpression/LambdaDemo2.java
+++ learning_java/learning_java/src/lambdaexpression/LambdaDemo2.java
@@ -13,7 +13,7 @@ public class LambdaDemo2 {
         NumericTest isEven = (n) -> (n % 2) == 0;
         
         if (isEven.test(10)) System.out.println("10 is even");
-        if (isEven.test(9)) System.out.println("9 is even");
+        if (!isEven.test(9)) System.out.println("9 is not even");
         
         /*
         Now, use a lambda expression that test if a number
@@ -22,7 +22,7 @@ public class LambdaDemo2 {
         NumericTest isNonNeg = (n) -> n >= 0;
         
         if (isNonNeg.test(1)) System.out.println("1 is non-negative");
-        if (isNonNeg.test(-1)) System.out.println("-1 is non-negative");
+        if (!isNonNeg.test(-1)) System.out.println("-1 is negative");
     }
     
 }
