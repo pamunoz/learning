33f15a230ce7b503c0ea802c87ddf08b3b5d26d6
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-21 16:01:29 -0300

    Move annotations to setters

--- hibernate/salon/src/main/java/com/pfariasmunoz/salon/model/entities/Employee.java
+++ hibernate/salon/src/main/java/com/pfariasmunoz/salon/model/entities/Employee.java
@@ -32,24 +32,22 @@ import javax.persistence.Table;
 @NamedQuery(name="Employee.findAll", query="SELECT i from Employee i")
 public class Employee implements Externalizable{
 
-    @Id
-    @GeneratedValue
-    @Column(name = "id", nullable = false)
+    
     private Long mId;
 
-    @Column(name = "first_name", nullable = false)
+    
     private String mFirstName;
 
-    @Column(name = "last_name", nullable = false)
+    
     private String mLastName;
 
-    @OneToMany(mappedBy = "mEmployee", cascade = {CascadeType.ALL})
+   
     private List<Schedule> mScheduleList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "mEmployeeCreated", cascade = {CascadeType.ALL})
+    
     private List<Appointment> mAppointmentsCreatedList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "mAssingEmployee", cascade = {CascadeType.ALL})
+    
     private List<Appointment> mAppointmentsAssignList = new ArrayList<>();
 
     public Employee() {
@@ -61,60 +59,84 @@ public class Employee implements Externalizable{
         this.mFirstName = mFirstName;
         this.mLastName = mLastName;
     }
+    
+    @Id @GeneratedValue
+    @Column(name = "id")
+    public Long getmId() {
+        return mId;
+    }
 
+    @Column(name = "first_name", nullable = false)
+    public String getmFirstName() {
+        return mFirstName;
+    }
+    
+    @Column(name = "last_name", nullable = false)
+    public String getmLastName() {
+        return mLastName;
+    }
+    
+    @OneToMany(mappedBy = "mEmployee", cascade = {CascadeType.ALL})
+    public List<Schedule> getmScheduleList() {
+        return mScheduleList;
+    }
+    
+    @OneToMany(mappedBy = "mEmployeeCreated", cascade = {CascadeType.ALL})
     public List<Appointment> getmAppointmentsCreatedList() {
         return mAppointmentsCreatedList;
+    }
+    
+    @OneToMany(mappedBy = "mAssingEmployee", cascade = {CascadeType.ALL})
+    public List<Appointment> getmAppointmentsAssignList() {
+        return mAppointmentsAssignList;
     }
 
     public void setmAppointmentsCreatedList(List<Appointment> mAppointmentsCreatedList) {
         this.mAppointmentsCreatedList = mAppointmentsCreatedList;
     }
 
-    public List<Appointment> getmAppointmentsAssignList() {
-        return mAppointmentsAssignList;
-    }
-
-    public void setmAppointmentsAssignList(List<Appointment> mAppointmentsAssignList) {
-        this.mAppointmentsAssignList = mAppointmentsAssignList;
-    }
-
+    
     public void addAppointment(Appointment appointment) {
         mAppointmentsCreatedList.add(appointment);
         appointment.setmEmployeeCreated(this);
+    }
+    
+    public void removeAppointment(Appointment appointment) {
+        mAppointmentsCreatedList.remove(appointment);
+        appointment.setmEmployeeCreated(null);
     }
 
     public void addSchedule(Schedule schedule) {
         mScheduleList.add(schedule);
         schedule.setmEmployee(this);
     }
+    
 
-    public String getmFirstName() {
-        return mFirstName;
+    public void setmAppointmentsAssignList(List<Appointment> mAppointmentsAssignList) {
+        this.mAppointmentsAssignList = mAppointmentsAssignList;
     }
+
+   
+
+    
 
     public void setmFirstName(String mFirstName) {
         this.mFirstName = mFirstName;
     }
 
-    public String getmLastName() {
-        return mLastName;
-    }
+    
 
     public void setmLastName(String mLastName) {
         this.mLastName = mLastName;
     }
 
-    public List<Schedule> getmScheduleList() {
-        return mScheduleList;
-    }
+    
 
     public void setmScheduleList(List<Schedule> mScheduleList) {
         this.mScheduleList = mScheduleList;
     }
 
-    public Long getmId() {
-        return mId;
-    }
+    
 
     public void setmId(Long mId) {
         this.mId = mId;
