e0d1ae93358874f9d3040807a5a91e25c4daa865
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-28 17:53:57 -0300

    Add squeletons for the controllers

--- chapter_3/src/com/pfariasmunoz/jfx8/ch3/AddEditUIController.java
+++ chapter_3/src/com/pfariasmunoz/jfx8/ch3/AddEditUIController.java
@@ -1,27 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.pfariasmunoz.jfx8.ch3;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
-/**
- * FXML Controller class
- *
- * @author pablo
- */
-public class AddEditUIController implements Initializable {
+public class AddEditUIController extends BaseController {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="newNotesBtn"
+    private Button newNotesBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cancelBtn"
+    private Button cancelBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="clearBtn"
+    private Button clearBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="saveBtn"
+    private Button saveBtn; // Value injected by FXMLLoader
+
+    @FXML
+    void handleCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleNewNotes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSave(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert newNotesBtn != null : "fx:id=\"newNotesBtn\" was not injected: check your FXML file 'AddEditUI.fxml'.";
+        assert cancelBtn != null : "fx:id=\"cancelBtn\" was not injected: check your FXML file 'AddEditUI.fxml'.";
+        assert clearBtn != null : "fx:id=\"clearBtn\" was not injected: check your FXML file 'AddEditUI.fxml'.";
+        assert saveBtn != null : "fx:id=\"saveBtn\" was not injected: check your FXML file 'AddEditUI.fxml'.";
+
+    }
 }
--- chapter_3/src/com/pfariasmunoz/jfx8/ch3/ListNotesUIController.java
+++ chapter_3/src/com/pfariasmunoz/jfx8/ch3/ListNotesUIController.java
@@ -1,35 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Sample Skeleton for 'ListNotesUI.fxml' Controller Class
  */
+
 package com.pfariasmunoz.jfx8.ch3;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
-/**
- *
- * @author pablo
- */
-public class ListNotesUIController implements Initializable {
-    
+public class ListNotesUIController extends BaseController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="newNoteBtn"
+    private Button newNoteBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="deleteNoteBtn"
+    private Button deleteNoteBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="editNoteBtn"
+    private Button editNoteBtn; // Value injected by FXMLLoader
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    void handleDeleteNote(ActionEvent event) {
+
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+    @FXML
+    void handleEditNote(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleNewNote(ActionEvent event) throws IOException {
+        editNoteBtn = null;
+        navigate(event, ADD.);
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert newNoteBtn != null : "fx:id=\"newNoteBtn\" was not injected: check your FXML file 'ListNotesUI.fxml'.";
+        assert deleteNoteBtn != null : "fx:id=\"deleteNoteBtn\" was not injected: check your FXML file 'ListNotesUI.fxml'.";
+        assert editNoteBtn != null : "fx:id=\"editNoteBtn\" was not injected: check your FXML file 'ListNotesUI.fxml'.";
+
+    }
 }
