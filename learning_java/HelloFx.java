ff56e8d2487ba092e0be811c93d4bbfa168b4fc2
Pablo Munoz <pfariasmunoz@gmail.com>
2017-02-27 16:22:48 -0300

    Add more detail to javaFx example project

--- learning_java/HelloFx.java
+++ learning_java/HelloFx.java
@@ -1,13 +1,20 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+
 /**
  * name
  */
 public class HelloFx extends Application {
-
+    @Override
     public void start(Stage stage) {
         stage.setTitle("Hello JavaFx");
+        Group group = new Group(new Label("Hello Lable"));
+        Scene scene = new Scene(group, 300, 500);
+        stage.setScene(scene);
         stage.show();
     }
 }
